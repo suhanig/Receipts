5369023852fa60759c24c158b56e0c76b4fa57ed
suhanig <60145970+suhanig@users.noreply.github.com>
2021-10-22 00:23:59 -0400

    made changes. confused on how to transfer the variables over from kiosk... to do for next time.

--- Printer.java
+++ Printer.java
@@ -20,12 +20,83 @@
 ****************************************
 */
 
-Public class Printer {
-  
-  public static void main (String[] args) {
-  
-  public Printer() {
-    
-    Kiosk myKiosk = new Kiosk;
-    System.out.println ("Walmart ");
-    System.out.print ("appleQuantity\t +
+public class Printer {
+	public Printer() {
+	
+	
+		
+	Kiosk myKiosk = new Kiosk(); //allows us to call on class Kiosk.java
+	
+	int frontHeader = 16; //will allow us to print 16 spaces before printing Walmart out
+	int backHeader = 17; //will allow us to print 17 spaces after printing Walmart out
+	int spacesBetweenFirst = 11; //spaces between Quantity and Item
+	int spacesBetweenSecond = 12; //spaces between Item and Price 
+	
+	while (frontHeader > 0) { //while loop for beginning spaces
+		System.out.print(" ");
+		frontHeader = frontHeader-1; //makes sure loop is not forever continuous
+	} //closes front spaces while loop
+	
+	System.out.print("Walmart");
+	
+	while (backHeader > 0) { //while loop for behind spaces
+		System.out.print(" ");
+		backHeader = backHeader -1; //makes sure loop is not forever continuous
+	} //closes behind spaces while loop
+	
+	System.out.print("\nQuantity");
+	
+	while (spacesBetweenFirst > 0) {
+		System.out.print(" ");
+		spacesBetweenFirst = spacesBetweenFirst -1; //makes sure loop is not forever continuous
+	}
+
+	System.out.print("Item");
+	
+	while (spacesBetweenSecond > 0) {
+		System.out.print(" ");
+		spacesBetweenSecond = spacesBetweenSecond -1; //makes sure loop is not forever continuous
+	}
+	
+	System.out.println("Price");
+	
+    //now calling on variables from the kiosk class. having issues with this. THINGS TO FIX: how to import variables from one class to another?
+	if (appleAmt > 0) {
+		double applePrice = apple * 1.99; //calculating price
+		System.out.print(appleAmt + "              " + "apple" + "              " + "$" + applePrice); //total of 40 char
+	}
+	if (orangeAmt > 0) {
+		double orangePrice = orange * 2.49; //calculating price
+		System.out.print(orangeAmt + "             " + "orange" + "              " + "$" + orangePrice); //total of 40 char
+	}
+	if (bananaAmt > 0) {
+		double bananaPrice = banana * 2.99; //calculating price
+		System.out.print(bananaAmt + "             " + "banana" + "              " + "$" + bananaPrice); //total of 40 char
+	}
+	if (watermelonAmt > 0) {
+		double watermelonPrice = watermelon * 3.49; //calculating price
+		System.out.print(watermelonAmt + "           " + "watermelon" + "            " + "$" + watermelonPrice); //total of 40 char
+	}
+	
+	if (lemonAmt > 0) {
+		double lemonPrice = lemon * 3.99; //calculating price
+		System.out.print(lemonAmt + "              " + "lemon" + "              " + "$" + lemonPrice); //total of 40 char
+	}
+	
+	myKiosk.Kiosk(); //thought would fetch variables... does it? can i put it in between the if...else... statement?
+	
+	else {
+		System.out.print("No items purchased!");
+	}
+	
+	System.out.println("\n Subtotal: " + subtotal); 
+	System.out.println("Tax: 7%");
+	System.out.println("Total: " + total);
+	
+	
+		
+		
+
+} //closes printer method
+} //closes printer class
+
